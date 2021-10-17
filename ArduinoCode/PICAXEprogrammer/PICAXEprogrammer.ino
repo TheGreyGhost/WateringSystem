c2e09790d11107f205d0200f688010fe8397583d
TheGreyGhost <5291905+TheGreyGhost@users.noreply.github.com>
2021-10-17 13:59:41 +1030

    Works robustly.
    The PICaxe Programmer code works robustly now

--- ArduinoCode/PICAXEprogrammer/PICAXEprogrammer.ino
+++ ArduinoCode/PICAXEprogrammer/PICAXEprogrammer.ino
@@ -2,12 +2,20 @@
 #include <DigitalIO.h>
 
 //  Assist with reprogramming PICAXE which aren't listening to programming commands
-// algorithm:
-// 1) in normal operation: passes serial information from PC to PICAXE and vica versa (direct pass-thru)
-// 2) during startup, sends frequent serial breaks to the PICAXE (high output for more than 1 frame; for 10 ms) every 20 ms
-// 3) when input is detected from the PC (manually done by user using terminal), drop back to 1 second intervals
-// 4) then, once further input is detected from the PC, stop sending breaks.
+//  I think the USB to serial interface I use also has trouble with sending serial BREAKS (serial signal raised to high for more than one frame, ~ 12 bits).
 
+// 1) SETUP:
+//   connect PICAXE pin C.5 (SerIn) to Arduino Pin 11 via 22K resistor
+//   connect PICAXE pin C.0 (SerOut) to Arduino Pin 10 via 22K resistor
+//   connect input from PC (normally connected to PICAXE pin C.5) to Arduino pin 8
+//   connect output to PC (normally connected to PICAXE pin C.0) to Arduino pin 9
+// 2) Apply power to the Arduino or press its reset button.  It will apply a continuous high to the PICAXE pin C.0, which the PICAXE interprets as a serial break
+// 3a) Open the PICAXE serial monitor and verify that the PICAXE is sending its identification data (repeats at ~ 1 second intervals)
+// 3b) If necessary: remove the PICAxe power (both GND and +5V) and restore.  Repeat until the identification data is being sent.
+// 4) Start the download.  When the Arduino detects the input signal from the PC, it sends a low to the PICAXE pin C.0 for 3 bits, then passes through the data from 
+//    the PC to the PICAXE.  Buffers these data to account for the 3 bit low duration which was inserted.  Also set the pin 13 LED to show that it is buffering.
+
+// pin allocation:
 // 8 = input from PC
 // 9 = output to PC
 // 10 = input from PICAXE
@@ -20,16 +28,11 @@
 //PORTB - The Port B Data Register - read/write
 //PINB - The Port B Input Pins Register - read only
 
-const long BREAK_US = 1000000;
-const long BREAK_SPACING_QUICK_US = 20000;
-const long BREAK_SPACING_SLOW_US = 200000;
+const int BIT_LENGTH_US = (int)(1000000ul/4800); // 1/4800 ~ 
 
 char outputs[8];
 
 void setup() {
-  Serial.begin(9600);
-//  while (!Serial) ; // wait for Arduino Serial Monitor to open
-//  Serial.println("Ready");
   DDRB = B11111010;  // pin 8 and pin 10 are inputs
 
   int r;
@@ -42,8 +45,68 @@ void setup() {
 }
 
 void loop() {
-  delaytest(BREAK_SPACING_QUICK_US, true);
+  ringbuffer();
 }
+
+const int BUFFER_SIZE = 256;
+int ringbufferDuration[BUFFER_SIZE];
+bool ringbufferState[BUFFER_SIZE];
+int headIdx = 0;
+int tailIdx = 0;
+
+void ringbuffer() {
+  bool serialbreak = true;
+  bool buffering = false;
+  int r;
+  long timenow, lasttime;
+  int delta;
+  bool inputdetected;
+  
+  do {
+    long timenow = micros();
+    if (buffering) {
+      delta = timenow - lasttime;   
+      while (tailIdx != headIdx && delta > ringbufferDuration[tailIdx]) {
+        delta -= ringbufferDuration[tailIdx];
+        tailIdx = (tailIdx + 1) & 0xff;
+      }
+      
+      ringbufferDuration[headIdx] += delta;
+      ringbufferDuration[tailIdx] -= delta;
+      r = PINB & B101;
+      inputdetected = (r & 1);
+      if (inputdetected != ringbufferState[headIdx]) {
+        headIdx = (headIdx + 1) & 0xff;
+        ringbufferDuration[headIdx] = 0;
+        ringbufferState[headIdx] = inputdetected;
+      }
+
+      if (ringbufferState[tailIdx]) {
+        r |= 1;    
+      } else {
+        r &= 0xfe; 
+      }
+      PORTB = (PORTB & B11000000) | outputs[r] | 32;
+    } else {
+      r = PINB & B101;
+      inputdetected = (r & 1);
+      if (serialbreak) r |= 1;
+      PORTB = (PORTB & B11000000) | outputs[r];
+      if (inputdetected) {
+        ringbufferState[headIdx] = false;
+        ringbufferDuration[headIdx] = 5 * BIT_LENGTH_US  ; headIdx = (headIdx + 1) & 0xff;
+        ringbufferState[headIdx] = true;
+        ringbufferDuration[headIdx] = 0;
+        buffering = true;
+      }
+    } 
+    lasttime = timenow;
+  } while (true);
+}
+
+const long BREAK_US = 1000000;
+const long BREAK_SPACING_QUICK_US = 20000;
+const long BREAK_SPACING_SLOW_US = 200000;
 
 // if infiniteloop is true: once PC input is detected, loop infinitely in passthrough
 void passthrough(long breakSpacingUs, bool infiniteloop) {
@@ -76,38 +139,4 @@ void passthrough(long breakSpacingUs, bool infiniteloop) {
     } while (true);
   }
   
-}
-
-// if infiniteloop is true: once PC input is detected, loop infinitely in passthrough
-void delaytest(long breakSpacingUs, bool infiniteloop) {
-  long pcInputTime, picaxeInputTime, timenow;
-  bool serialbreak;
-  int r;
-  bool pcInputDetected, picaxeInputDetected;
-  serialbreak = false;  
-
-  do {
-    timenow = micros();
-    r = PINB & B101;
-    pcInputDetected = (r & 1);
-    picaxeInputDetected = (r & 4);
-    if (picaxeInputDetected) {
-      picaxeInputTime = timenow;
-    }
-    if (pcInputDetected) {
-      pcInputTime = timenow;
-    }
-    r |= 1;
-    PORTB = (PORTB & B11000000) | outputs[r];
-  } while (!pcInputDetected);
-
-  long milliDelta = (pcInputTime - picaxeInputTime) / 1000;
-  Serial.println((int)milliDelta, DEC);
-
-  if (infiniteloop) {
-    do {
-      r = PINB & B101;
-      PORTB = (PORTB & B11000000) | outputs[r] | 32;
-    } while (true);
-  }
 }
